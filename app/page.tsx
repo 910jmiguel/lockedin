1ec5e290723b536c8eb83051859062fe8545b0ee
ItsAdamLY <itsadamly@gmail.com>
2025-08-20 01:03:54 +0800

    update to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-[url(/img/stinky.png)] bg-cover bg-center bg-no-repeat">
-          <header>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16">
+      <main className="rounded-xl flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-[url(/img/stinky.png)] bg-cover bg-center bg-no-repeat">
+          <header className="p-8">
               {/*<Image src="/img/stinky.png" alt="stinky" width={'200'} height={'200'} />*/}
               <h1 className="header text-left">
                   <span className={"font-geist-mono font-bold text-sky-500"} style={{fontStyle: 'bold', fontSize: '80px'}}>LockedIn</span>
@@ -12,6 +12,11 @@ export default function Home() {
               <h2>
                   <span className={"font-geist-mono font-medium"} style={{fontSize: '20px'}}>Your personal student dashboard app.</span>
               </h2>
+              <button>
+                  <a href="/getstarted" className="bg-sky-500 m:8 text-white px-4 py-2 rounded hover:bg-sky-600 transition-colors">
+                      Get Started
+                  </a>
+              </button>
           </header>
           {/*
           <div className="flex gap-[32px]" style={{fontSize: '25px'}}>
