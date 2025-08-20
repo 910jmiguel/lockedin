d3c469f05a86ae670fc0b965ed761a3250276f64
910jmiguel <910jmiguel@gmail.com>
2025-08-20 16:38:42 -0400

    feat: update Dashboard layout and integrate QuickDashboard component

---
+++ app/components/QuickDashboard.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../components/ui/button";
+
+const QuickDashboard = () => {
+  return (
+    <div>
+        <Button>Courses</Button>
+        <Button>Contacts</Button>
+        <Button>Extra Notes</Button>
+        <Button>Settings</Button>
+    </div>
+  )
+}
+
+export default QuickDashboard
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,16 @@
 import { Logout } from "../components/logout";
+import QuickDashboard from "../components/QuickDashboard";
 
 export default function Dashboard() {
     return (
-        <div>
+        <div className="font-inter mx-30 mt-20">
             <Logout />
+
+            {/* Main title of dashboard */}
+            <div>
+                <h1 className="text-4xl font-bold">Student Dashboard</h1>
+                <QuickDashboard />
+            </div>
         </div>
     )
 }
