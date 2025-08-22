0267b6dc5abbc115857d8bb89fb42900e474d7dd
ItsAdamLY <itsadamly@gmail.com>
2025-08-22 23:30:41 +0800

    commit: some bit of backend app-db routing stuff

---
+++ app/api/courses/[id]/tasks/route.ts
@@ -0,0 +1,35 @@
+﻿import { db } from "@/db/drizzle";
+import { courses, tasks } from "@/db/schema";
+
+export async function POST(request: Request, { params }: { params: { id: string } }) {
+
+  const courseId = parseInt(params.id, 10); // radix means numerical base - adamly
+  const { title, description, dueDate } = await request.json();
+
+  const [ task ] = await db.insert(tasks).values({
+    courseId,
+    title,
+    description,
+    dueDate: dueDate ? new Date(dueDate) : null,
+    status: status ? status : "not started",
+  }).returning();
+
+  return Response.json(task);
+
+}
+
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+
+  const taskId = parseInt(params.id, 10);
+  const { title, description, dueDate, status } = await request.json();
+
+  const [ task ] = await db.update(tasks).set({
+    title,
+    description,
+    dueDate: dueDate ? new Date(dueDate) : null,
+    status,
+  }).where(tasks.id.equals(taskId)).returning();
+
+  return Response.json(task);
+
+}
---
+++ app/api/courses/route.ts
@@ -0,0 +1,13 @@
+﻿import { db } from "@/db/drizzle";
+import {courses} from "@/db/schema";
+
+export async function POST(request: Request) {
+
+    const { name } = await request.json();
+
+    const [ course ] = await db.insert(courses).values({
+        name // this is the only required field
+    }).returning();
+
+    return Response.json(course);
+}
